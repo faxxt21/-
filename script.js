b8c5d8991b491b56db3536389661261f9f752517
Alina <lina@MacBook-Air-Alina.local>
2025-11-25 04:16:29 +0200

    Added JavaScript and validation

---
+++ script.js
@@ -0,0 +1,18 @@
+const form = document.querySelector("form");
+    const emailInput = document.querySelector('input[type="email"]');
+    const submitBtn = document.querySelector('button[type="submit"]');
+    if (form) {
+        form.addEventListener("submit", function (event) {
+            event.preventDefault();
+            const emailValue = emailInput.value.trim();
+            if (emailValue.includes("@") && emailValue.includes(".")) {
+                alert(`Чудово! Email "${emailValue}" пройшов перевірку.`);
+                emailInput.value = "";
+                emailInput.classList.remove("is-invalid");
+                emailInput.classList.add("is-valid");
+            } else {
+                alert("Помилка: Введіть коректний Email (має бути @ та крапка).");
+                emailInput.classList.add("is-invalid");
+            }
+        });
+    }
